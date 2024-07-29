a95672f4c149695742c28264e784462b31da3555
Clara Mauro <maurog.clara@gmail.com>
2024-07-29 17:54:40 +0400

    integration homepage ok, ajout accessibilité et SEO

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
+// Ouvrir/Fermer le menu de navigation mobile
 const menuBtn = document.querySelector("#btn-menu");
 const mobileMenu = document.querySelector("#mobile-menu");
-
 menuBtn.addEventListener("click", (e) => {
-    mobileMenu.classList.toggle("hidden");
+    if (mobileMenu.classList.contains("hidden")) {
+        mobileMenu.classList.remove("hidden");
+        mobileMenu.setAttribute("aria-hidden", "false");
+        menuBtn.setAttribute("aria-expanded", "true");
+    } else {
+        mobileMenu.classList.add("hidden");
+        mobileMenu.setAttribute("aria-hidden", "true");
+        menuBtn.setAttribute("aria-expanded", "false");
+    }
 });
