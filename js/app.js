3d6a09b7594204c07cacb2752b17e49186434ddf
Clara Mauro <maurog.clara@gmail.com>
2024-07-30 13:06:58 +0400

    séparation des fichiers CSS, JS front passé en module

--- js/app.js
+++ js/app.js
@@ -1,14 +1,21 @@
-// Ouvrir/Fermer le menu de navigation mobile
-const menuBtn = document.querySelector("#btn-menu");
-const mobileMenu = document.querySelector("#mobile-menu");
-menuBtn.addEventListener("click", (e) => {
-    if (mobileMenu.classList.contains("hidden")) {
-        mobileMenu.classList.remove("hidden");
-        mobileMenu.setAttribute("aria-hidden", "false");
-        menuBtn.setAttribute("aria-expanded", "true");
-    } else {
-        mobileMenu.classList.add("hidden");
-        mobileMenu.setAttribute("aria-hidden", "true");
-        menuBtn.setAttribute("aria-expanded", "false");
-    }
-});
+const app = {
+    init: () => {
+        const menuBtn = document.querySelector("#btn-menu");
+        menuBtn.addEventListener("click", app.handleClickOnBurgerIcon);
+
+        app.mobileMenu = document.querySelector("#mobile-menu");
+    },
+    handleClickOnBurgerIcon: (e) => {
+        if (app.mobileMenu.classList.contains("hidden")) {
+            app.mobileMenu.classList.remove("hidden");
+            app.mobileMenu.setAttribute("aria-hidden", "false");
+            e.currentTarget.setAttribute("aria-expanded", "true");
+        } else {
+            app.mobileMenu.classList.add("hidden");
+            app.mobileMenu.setAttribute("aria-hidden", "true");
+            e.currentTarget.setAttribute("aria-expanded", "false");
+        }
+    },
+};
+
+app.init();
